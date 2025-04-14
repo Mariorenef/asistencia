3483c5b452777cf9a37cf7c83b60968e2ecc64e5
Mariorenef <mariofigueroa1348@gmail.com>
2025-04-14 00:56:59 -0300

    mejoras

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+
+// vite.config.ts o vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/asistencia/', // 👈 reemplazá por el nombre real de tu repo
   plugins: [react()],
 })
